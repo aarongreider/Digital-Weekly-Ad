2a092d39646a71b852eee7668004cceb86859ab2
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2023-08-22 11:58:31 -0400

    Completed item overlay cards

--- adData_banners.js
+++ adData_banners.js
@@ -10,57 +10,12 @@ fetch('https://script.google.com/macros/s/AKfycbyKAzmaxBAnyrpyW9QTW_XKIWQdWPrjtp
         console.log(response);
         jsonToBanners(response)
         jsonToOverlays(response);
+        setListeners();
     })
     .catch(error => {
         // Handle errors here
         console.error('Fetch error:', error);
     });
-
-    // add event listen to .info to toggle overlays
-
-function getBannerFrag(title, price, save, img) {
-    const banner = document.createElement('template');
-    let fragment = `
-            <div class="banner">
-				<div class="buttonContainer">
-					<p class="info">i</p>
-					<p class="add">+</p>
-				</div>
-				<div class="description">
-					<p>${title}</p>
-				    <div class="triangle"></div>
-                </div>
-                
-			</div>`;
-            
-    /* IMPORTANT
-     * in the future use createDocumentFragment() instead of innerHTML to avoid potential XSS attacks :/
-     * could also use DOMPurify api
-     */
-    banner.innerHTML = fragment;
-    return banner;
-}
-
-function getOverlayFrag(title, price, save, img) {
-    const overlay = document.createElement('template');
-    let fragment = `
-            <div class="overlay">
-                <img
-                    src="${img}">
-                <div class="cardTextContainer">
-                    <h2>${title}</h2>
-                    <p>${price}</p>
-                    <p>Save ${save}</p>
-                </div>
-            </div>`;
-            
-    /* IMPORTANT
-     * in the future use createDocumentFragment() instead of innerHTML to avoid potential XSS attacks :/
-     * could also use DOMPurify api
-     */
-    overlay.innerHTML = fragment;
-    return overlay;
-}
 
 function jsonToBanners(response) {
     // instead of creating a div, cycle through each item and append the banner to it
@@ -76,12 +31,84 @@ function jsonToBanners(response) {
 
 function jsonToOverlays(response) {
     // instead of creating a div, cycle through each item and append the banner to it
-    let items = document.getElementsByClassName("info");
+    //let items = document.getElementsByClassName("info");
+    let container = document.getElementById('overlayContainer');
 
     for (let i = 0; i < response.data.length; i++) {
         let itemData = response.data[i];
         console.log(itemData["Product Description"])
         let overlay = getOverlayFrag(itemData["Product Description"], itemData["Cost"], itemData["Save"], itemData["Image"])
-        items[i].append(overlay.content);
+        container.append(overlay.content);
     }
 }
+
+function getBannerFrag(title, price, save, img) {
+    const banner = document.createElement('template');
+    let fragment = `
+            <div class="banner">
+				<div class="buttonContainer">
+					<p class="info">i</p>
+					<p class="add">+</p>
+				</div>
+				<div class="description">
+					<p>${title}</p>
+				    <div class="triangle"></div>
+                </div>
+                
+			</div>`;
+
+    /* IMPORTANT
+     * in the future use createDocumentFragment() instead of innerHTML to avoid potential XSS attacks :/
+     * could also use DOMPurify api
+     */
+    banner.innerHTML = fragment;
+    return banner;
+}
+
+function getOverlayFrag(title, price, save, img) {
+    const overlay = document.createElement('template');
+    let fragment = `
+            <div class="overlay">
+                <p class="backButton">&larr;</p>
+                <img
+                    src="${img}">
+                <div class="cardTextContainer">
+                    <h2>${title}</h2>
+                    <p>${price}</p>
+                    <p>Save ${save}</p>
+                </div>
+            </div>`;
+
+    /* IMPORTANT
+     * in the future use createDocumentFragment() instead of innerHTML to avoid potential XSS attacks :/
+     * could also use DOMPurify api
+     */
+    overlay.innerHTML = fragment;
+    return overlay;
+}
+
+// add event listen to .info to toggle overlays
+function setListeners() {
+    Array.from(document.getElementsByClassName('info')).forEach(function (button, i) {
+        button.addEventListener('click', () => {
+            console.log('info click')
+            displayOverlay(i);
+        });
+    });
+
+    document.getElementById('overlayContainer').addEventListener('click', () => {
+        console.log('overlaycontainer click')
+        Array.from(document.getElementsByClassName('overlayActive')).forEach(function (overlay) {
+            overlay.classList.toggle('overlayActive');
+        });
+    })
+}
+
+function displayOverlay(index) {
+    let overlays = document.getElementsByClassName('overlay');
+    let container = document.getElementById('overlayContainer');
+    overlays[index].classList.toggle('overlayActive');
+    container.classList.toggle('overlayActive');
+}
+
+
